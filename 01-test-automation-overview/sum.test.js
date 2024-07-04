cee945d60c94368d53543c7f485c1f629075ac16
Dimitar Nikolov <d.n.nikolov@protonmail.com>
2024-07-04 09:49:29 +0300

    Add more tests to sum.js

--- 01-test-automation-overview/sum.test.js
+++ 01-test-automation-overview/sum.test.js
@@ -4,6 +4,23 @@ test('Sum of [1, 2] should be 3', () => {
     expect(Sum([1, 2])).toBe(3);
 });
 
+test('Sum of [5, 5] should be 10', () => {
+    expect(Sum([5, 5])).toBe(10);
+});
+
+test('Sum of [1, 2] should not be 5', () => {
+    expect(Sum([1, 2])).not.toBe(5);
+});
+
 test('Sum of an empty array should be 0', () => {
     expect(Sum([])).toBe(0);
+});
+
+test('Sum of an 1 should be 1', () => {
+    expect(Sum([1])).toBe(1);
+});
+
+//this one must fail!
+test('Sum of [1, 1] should be 2', () => {
+    expect(Sum([1, 1])).toBe(3);
 });
